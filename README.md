a58be088d8e65f834e8e44798d53c1c70c250c88
Orman <corman.dev@gmail.com>
2022-01-02 17:53:00 -0300

    :zap: initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Frugal Piggybank Web Client
+
+React web client for the [frugal piggybank](https://github.com/Frugal-Piggybank)
+
+## Installation
+
+```bash
+yarn install
+```
+
+## Run the project locally
+
+```bash
+yarn tsc
+yarn dev
+```
+
+Navigate to http://localhost:3000/
---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+import { AtSignIcon, CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Hero } from "../components/hero";
+import { Container } from "../components/container";
+import { Main } from "../components/main";
+import { DarkModeSwitch } from "../components/dark-mode-switch";
+import { Footer } from "../components/footer";
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero title="Frugal Piggybank" />
+    <Main>
+      <Text textAlign={"center"}>An easy place to manage your budget.</Text>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text bgClip="text" bgGradient="linear(to-l, #7928CA, #FF0080)">
+        @thefrugaldev
+      </Text>
+    </Footer>
+  </Container>
+);
+
+export default Index;
